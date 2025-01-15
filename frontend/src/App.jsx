9c3b4d75f6b76e4cdedca38b9f8594dc64cce56c
Souvik <souviksarkar260123@gmai.com>
2025-01-15 08:44:37 +0530

    setup routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import Price from "./pages/Price";
+
 const App = () => {
-  return <div className=" border text-3xl underline">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
